00e28dd59c311b0c038c83031c543b8c962b40ba
jolan27 <163869453+jolan27@users.noreply.github.com>
2024-11-13 16:36:06 +0100

    Ajout des derniere couleurs et modif bouton retour

--- Menu/Paramètres.js
+++ Menu/Paramètres.js
@@ -1,18 +1,43 @@
 var couleurJ1 ="blue"
-
+var bouton = document.querySelectorAll("button")
+var Head = document.querySelectorAll("header")
 var deconnexion = document.getElementById("Retour")
+var rouge = document.getElementById("red");
+var vert = document.getElementById("green");
+var bleu = document.getElementById("blue");
 
 deconnexion.addEventListener("click", () => {
     window.location.href = "../Menu/Menu.html"
 })
 
-var rouge = document.getElementById("red");
+function color () {
+    bouton.forEach(boutons => {
+        boutons.style.backgroundColor = couleurJ1
+    })
+
+    Head.forEach(Heads => {
+        Heads.style.backgroundColor = couleurJ1
+    })
+    
+}
 
 rouge.addEventListener("click", () => {
     couleurJ1 = "red"
+    color();
 })
 
-rouge.addEventListener("click", () => {
-    var Header = document.getElementById("head")
-    Header.style.backgroundColor = "red";
+vert.addEventListener("click", () => {
+    couleurJ1 = "green"
+    color();
 })
+
+bleu.addEventListener("click", () => {
+    couleurJ1 = "blue"
+    color();
+})
+// rouge.addEventListener("click", () => {
+//     var Header = document.getElementById("head")
+//     Header.style.backgroundColor = "red";
+// })
+
+console.log(couleurJ1)
